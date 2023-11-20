5ae1c128273abeb933efacc970a320d2d836e118
Ranvir <tylerturdenrgp@gmail.com>
2023-11-20 15:44:58 +0530

    flutter get

---
+++ android/app/src/main/kotlin/com/example/safeharbor/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.safeharbor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+      ),
+      initialRoute:',
+      routes: {
+        //'/profile': (context) => ProfilePage(),
+      },
+    );
+  }
+}
